ae121892e7b3b8d529136ab27b1e8eb5ebe11dae
zhouuualexander <zzhou3@ualberta.ca>
2021-04-22 18:59:31 +0800

    add some comments

--- crazymorty.py
+++ crazymorty.py
@@ -44,98 +44,133 @@ class Rick:
 
 
 class Morty:
+    """Object Morty
+    """
+
     def __init__(self, parent_screen, length):
-        
+        """Initialize Object Morty and set the first position on the screen
+        Args:
+            parent_screen (surface): pygame surface(the screen)
+            length: Morty's length (the number of Mortys)
+        """
+        # Set Morty's length from the given parameter length
         self.length = length
+        # Initialize parent_screen
         self.parent_screen = parent_screen
-        self.block = pygame.image.load(
+        # load image of Morty
+        self.morty = pygame.image.load(
             "resources/rickandmorty/morty.png").convert()
+        # Set the position of Morty as a form of a LIST
         self.block_x = [10*SIZE+10]*length
         self.block_y = [9*SIZE+10]*length
-
+        # Set initial moving position of Morty to 'down'
         self.direction = 'down'
+        # Initialize all the direction switch to True, which means Morty can change to move to any direction at this time
+        # Direction will change to False when Morty should not go that way, i.e. when Morty's length is 2, after moving up, Morty cannot go down immediately
         self.right = True
         self.left = True
         self.up = True
         self.down = True
 
     def increace_length(self):
+        """Increase the length of Morty when killed one Rick
+        """
+        # Increase Morty's length by one pixel each time
         self.length += 1
+        # Append one element to the list of Morty, i.e.  self.block_x = [10*SIZE+10]*length, self.block_y = [9*SIZE+10]*length
         self.block_x.append(-1)
         self.block_y.append(-1)
 
     def draw(self):
+        """Draw Mortys on the screen
+        """
+        # Set screen's color to be white
         self.parent_screen.fill((255, 255, 255))
-        pygame.draw.rect(self.parent_screen,(0,0,0),(0,132,1120,812),10)
-       
-        
-
+        # Draw a rectangular to be a boundary of the game, i.e., if Morty surpass the boundary, game over.
+        pygame.draw.rect(self.parent_screen, (0, 0, 0),
+                         (0, 132, 1120, 812), 10)
+        # Draw Mortys according to the length
         for i in range(self.length):
             self.parent_screen.blit(
-                self.block, (self.block_x[i], self.block_y[i]))
+                self.morty, (self.block_x[i], self.block_y[i]))
         # pygame.display.flip()
 
     def move_up(self):
+        """Move Morty up
+        """
+        # Change Morty's direction to up
         self.direction = 'up'
-        
-        if self.length>1:
+        # If Morty's length is greater than 1, Morty cannot move to the opposite direction immediately.
+        if self.length > 1:
+            # Control the direction switch
+            # Disable the switch for moving down
             self.right = True
             self.left = True
             self.up = True
             self.down = False
-        
-
 
     def move_down(self):
+        """Move Morty down
+        """
+        # Change Morty's direction to Down
         self.direction = 'down'
-        
-        if self.length>1:
+        # If Morty's length is greater than 1, Morty cannot move to the opposite direction immediately.
+        if self.length > 1:
+            # Control the direction switch
+            # Disable the switch for moving up
             self.right = True
             self.left = True
             self.up = False
             self.down = True
-        
 
     def move_left(self):
+        # Change Morty's direction to Left
         self.direction = 'left'
-        
-        if self.length>1:
+        # If Morty's length is greater than 1, Morty cannot move to the opposite direction immediately.
+        if self.length > 1:
+            # Control the direction switch
+            # Disable the switch for moving right
             self.right = False
             self.left = True
             self.up = True
             self.down = True
-       
 
     def move_right(self):
+        # Change Morty's direction to Right
         self.direction = 'right'
-        
-        if self.length>1:
+        # If Morty's length is greater than 1, Morty cannot move to the opposite direction immediately.
+        if self.length > 1:
+            # Control the direction switch
+            # Disable the switch for moving left
             self.right = True
             self.left = False
             self.up = True
             self.down = True
-        
 
     def walk(self):
+        """Walk Mortys
+        """
+        # Use a loop to move the Morty to the position of the previous Morty
         for i in range(self.length-1, 0, -1):
             self.block_x[i] = self.block_x[i-1]
             self.block_y[i] = self.block_y[i-1]
         if self.direction == 'up':
-            if self.length>1:
-                self.down = False
+            # if self.length > 1:
+            #     self.down = False
             self.block_y[0] -= SIZE
         if self.direction == 'down':
-            if self.length>1:
-                self.up = False
+            # if self.length > 1:
+            #     self.up = False
             self.block_y[0] += SIZE
         if self.direction == 'left':
-            if self.length>1:
-                self.right = False
+            # if self.length > 1:
+            #     self.right = False
             self.block_x[0] -= SIZE
         if self.direction == 'right':
-            if self.length>1:
-                self.left = False
+            # if self.length > 1:
+            #     self.left = False
             self.block_x[0] += SIZE
+        # Draw Mortys
         self.draw()
 
 
@@ -148,7 +183,7 @@ class Game:
         self.speed_level = 1
         self.speed = 0.3
         self.firstpageImg = pygame.image.load("resources/img/Startpage.jpeg")
-        
+
         self.surface.fill((255, 255, 255))
         self.play_background_music()
         self.morty = Morty(self.surface, 1)
@@ -156,7 +191,7 @@ class Game:
         self.rick = Rick(self.surface)
         self.rick.draw()
         self.zen_mode = False
-        
+
         try:
             self.f = open("best_score.txt", "r")
             self.best_score = int(self.f.read())
@@ -166,7 +201,6 @@ class Game:
             self.f.write('0')
             self.best_score = 0
             self.f.close()
-
 
     def is_collision(self, x1, y1, x2, y2):
         if x1 >= x2 and x1 < x2+SIZE:
@@ -179,6 +213,7 @@ class Game:
             "resources/rickandmorty/rickandmorty.mp3")
         pygame.mixer.music.play(loops=-1)
         pygame.mixer.music.set_volume(1)
+
     def play_background_music(self):
         pygame.mixer.music.load(
             "resources/rickandmorty/rickandmorty_beginning.mp3")
@@ -186,14 +221,14 @@ class Game:
         pygame.mixer.music.set_volume(0.5)
 
     def play(self):
-        
+
         if self.morty.length-1 > self.best_score:
             self.best_score = self.morty.length-1
         self.morty.walk()
         self.rick.draw()
         self.display_speed()
         self.display_score()
-        self.display_title() 
+        self.display_title()
         pygame.display.flip()
         # Morty killing Rick
         if self.is_collision(self.morty.block_x[0], self.morty.block_y[0], self.rick.block_x, self.rick.block_y):
@@ -201,22 +236,19 @@ class Game:
             rick_sounds = str(random.randint(1, 13))
             self.play_rick_sound(rick_sounds)
             self.morty.increace_length()
-            x,y = self.rick.move()
+            x, y = self.rick.move()
             occupied = True
-            #Check whether Rick's new position is conflit with Mortys,if yes, randomly generate a new position for Rick until no conflit happen.
+            # Check whether Rick's new position is conflit with Mortys,if yes, randomly generate a new position for Rick until no conflit happen.
             while occupied:
                 for i in range(0, self.morty.length):
                     if self.is_collision(x, y, self.morty.block_x[i], self.morty.block_y[i]):
-                        x,y = self.rick.move()
+                        x, y = self.rick.move()
                         occupied = True
                         count = 1
                 if count == 0:
                     occupied = False
                 count = 0
-                
 
-
-            
         # Morty killing himself
         for i in range(3, self.morty.length):
             if self.is_collision(self.morty.block_x[0], self.morty.block_y[0], self.morty.block_x[i], self.morty.block_y[i]):
@@ -226,74 +258,71 @@ class Game:
         if not (0 <= self.morty.block_x[0] <= 1100 and 142 <= self.morty.block_y[0] <= 768):
             self.play_sound('LOSE')
             raise "Hit the boundry error"
+
     def pause(self):
         self.speed = 0.3
         self.speed_level = 1
         self.surface.fill((255, 255, 255))
-        font = pygame.font.Font('resources/font/rick_and_morty.ttf',30)
+        font = pygame.font.Font('resources/font/rick_and_morty.ttf', 30)
         line1 = font.render(
             f"Pause for a second and press Enter to continue", True, (
                 0, 0, 0))
         self.surface.blit(line1, (300, 350))
-        
+
         pygame.display.flip()
 
         pygame.mixer.music.pause()
+
     def display_title(self):
         if not self.zen_mode:
-            font = pygame.font.Font('resources/font/get_schwifty.ttf',50)
+            font = pygame.font.Font('resources/font/get_schwifty.ttf', 50)
             title = font.render(
-                    "Crazy  Morty", True, (0,0,0)
+                "Crazy  Morty", True, (0, 0, 0)
             )
         if self.zen_mode:
-            font = pygame.font.Font('resources/font/get_schwifty.ttf',50)
+            font = pygame.font.Font('resources/font/get_schwifty.ttf', 50)
             title = font.render(
-                    "Zen  Mode", True, (0,0,0)
+                "Zen  Mode", True, (0, 0, 0)
             )
         self.surface.blit(title, (120, 30))
+
     def display_speed(self):
-        
+
         if self.zen_mode:
-            font = pygame.font.Font('resources/font/rick_and_morty.ttf',30)
+            font = pygame.font.Font('resources/font/rick_and_morty.ttf', 30)
             speed_up = font.render(
-                    "To speed up Press F", True, (0,0,0)
+                "To speed up Press F", True, (0, 0, 0)
             )
             self.surface.blit(speed_up, (500, 30))
             speed_down = font.render(
-                    "To slow down Press S", True, (0,0,0)
+                "To slow down Press S", True, (0, 0, 0)
             )
             self.surface.blit(speed_down, (500, 80))
             show_speed = font.render(
-                    f"Speed: {self.speed_level}", True, (0,0,0)
+                f"Speed: {self.speed_level}", True, (0, 0, 0)
             )
             self.surface.blit(show_speed, (550, 55))
 
-
-
-
     def display_score(self):
-        font = pygame.font.Font('resources/font/rick_and_morty.ttf',30)
+        font = pygame.font.Font('resources/font/rick_and_morty.ttf', 30)
         numberRicks = self.morty.length-1
-        if numberRicks>1:
+        if numberRicks > 1:
             score = font.render(
                 f"Morty killed: {numberRicks} Ricks", True, (0, 0, 0))
             self.surface.blit(score, (800, 30))
         score = font.render(
-                f"Morty killed: {numberRicks} Rick", True, (0, 0, 0))
+            f"Morty killed: {numberRicks} Rick", True, (0, 0, 0))
         self.surface.blit(score, (800, 30))
 
         best = font.render(
-                f"Best killed {self.best_score} ", True, (0,0,0)
+            f"Best killed {self.best_score} ", True, (0, 0, 0)
         )
         self.surface.blit(best, (800, 80))
-        
-        
 
-
-
-    def play_rick_sound(self,sound):
+    def play_rick_sound(self, sound):
         sound = pygame.mixer.Sound(f"resources/rick_sounds/{sound}.mp3")
         pygame.mixer.Sound.play(sound)
+
     def play_sound(self, sound):
         sound = pygame.mixer.Sound(f"resources/rickandmorty/{sound}.mp3")
         pygame.mixer.Sound.play(sound)
@@ -308,7 +337,7 @@ class Game:
         f.close()
 
         self.surface.fill((0, 0, 0))
-        font = pygame.font.Font('resources/font/rick_and_morty.ttf',30)
+        font = pygame.font.Font('resources/font/rick_and_morty.ttf', 30)
         line1 = font.render(
             f"Wubba Lubba Dub-Dub!!! You just killed {self.morty.length-1} Rick", True, (
                 255, 255, 255))
@@ -328,10 +357,11 @@ class Game:
         self.rick = Rick(self.surface)
         self.speed_level = 1
         self.speed = 0.3
+
     def startpage(self):
         self.play_background_music()
-        self.surface.blit(self.firstpageImg, (0,0))
-        prompt_font = pygame.font.Font('resources/font/rick_and_morty.ttf',30)
+        self.surface.blit(self.firstpageImg, (0, 0))
+        prompt_font = pygame.font.Font('resources/font/rick_and_morty.ttf', 30)
         prompt1 = prompt_font.render(
             "To start killing Rick, press Enter", True, (
                 255, 255, 255))
@@ -341,37 +371,37 @@ class Game:
                 255, 255, 255))
         self.surface.blit(prompt2, (425, 300))
         zen = prompt_font.render(
-            "Zen Mode, press Z", True, (255,255,255)
+            "Zen Mode, press Z", True, (255, 255, 255)
         )
         self.surface.blit(zen, (100, 500))
-        title_font = pygame.font.Font('resources/font/get_schwifty.ttf',50)
-        title = title_font.render("Crazy Morty", True, (225,225,225))
-        self.surface.blit(title, (450,100))
+        title_font = pygame.font.Font('resources/font/get_schwifty.ttf', 50)
+        title = title_font.render("Crazy Morty", True, (225, 225, 225))
+        self.surface.blit(title, (450, 100))
         pygame.display.flip()
         start = True
         while start:
             for event in pygame.event.get():
                 if event.type == KEYDOWN:
-                        if event.key == K_RETURN:
-                            self.reset()
-                            self.play_sound("KILL")
-                            return True
-                        if event.key == K_z:
-                            self.reset()
-                            self.zen_mode = True
-                            
-                            self.play_sound("KILL")
-                            return True
-                        if event.key == K_ESCAPE:
-                            self.play_sound("QUIT")
-                            time.sleep(1.2)
-                            return False
+                    if event.key == K_RETURN:
+                        self.reset()
+                        self.play_sound("KILL")
+                        return True
+                    if event.key == K_z:
+                        self.reset()
+                        self.zen_mode = True
+
+                        self.play_sound("KILL")
+                        return True
+                    if event.key == K_ESCAPE:
+                        self.play_sound("QUIT")
+                        time.sleep(1.2)
+                        return False
                 elif event.type == QUIT:
                     self.play_sound("QUIT")
                     time.sleep(1.2)
                     return False
 
-    def run(self,running):
+    def run(self, running):
         self.play_game_music()
 
         # running = True
@@ -381,11 +411,11 @@ class Game:
                 if event.type == KEYDOWN:
                     if self.zen_mode:
                         if event.key == K_f:
-                            if self.speed >0.1:
-                                self.speed -=0.01
+                            if self.speed > 0.1:
+                                self.speed -= 0.01
                                 self.speed_level += 1
                         if event.key == K_s:
-                            if self.speed< 0.3:
+                            if self.speed < 0.3:
                                 self.speed += 0.01
                                 self.speed_level -= 1
                     if event.key == K_SPACE:
@@ -394,7 +424,7 @@ class Game:
                         pause = True
                     if event.key == K_ESCAPE:
                         self.play_sound("GIVEUP")
-                        
+
                         running = False
                     if event.key == K_RETURN:
                         self.speed = 0.3
